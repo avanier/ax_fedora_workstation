ceda19354d5c8da9e1da491f196ebf7d83cbe9a4
Alexis Vanier <alexis@amonoid.io>
2018-04-16 00:36:29 -0400

    Fix some pam bug in os-hardening

--- recipes/hardening.rb
+++ recipes/hardening.rb
@@ -6,3 +6,12 @@ node.default['os-hardening']['network']['forwarding'] = true
 
 include_recipe 'os-hardening'
 include_recipe 'ssh-hardening'
+
+# This hack is evil, it dynamically replace the wrapped cookbook's template
+# Check some documentation below
+# https://coderanger.net/rewind/
+# https://www.sethvargo.com/changing-chef-resources-at-runtime/
+edit_resource!(:template, '/etc/pam.d/system-auth-ac') do
+  source 'rhel_system_auth.erb'
+  cookbook 'ax_fedora_workstation'
+end
