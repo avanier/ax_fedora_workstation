4588dc09ab46c7094ef0ca5ae1ce6f733874d1b6
Alexis Vanier <alexis@amonoid.io>
2018-05-21 08:38:46 -0400

    Install golang and a few fun packages

--- recipes/development_tools.rb
+++ recipes/development_tools.rb
@@ -1,22 +1,11 @@
-rofi_build_deps = %w[
-  pango-devel
-  cairo-devel
-  glib2
+node.default['go']['version'] = '1.10.2'
+
+include_recipe 'golang'
+
+node.default['go']['packages'] = %w[
+  github.com/wallix/awless
+  github.com/adeven/go-wrk
+  github.com/justwatchcom/gopass
 ]
 
-# This is sketchy on purpose, sue me.
-local_variables.map(&:to_s).each do |v|
-  next unless v =~ /_deps$/
-  package "install_#{v}" do
-    package_name binding.local_variable_get(v)
-  end
-end
-
-yum_repository 'vscode' do
-  description 'Visual Studio Code'
-  baseurl 'https://packages.microsoft.com/yumrepos/vscode'
-  gpgkey 'https://packages.microsoft.com/keys/microsoft.asc'
-  action :create
-end
-
-package 'code'
+include_recipe 'golang::packages'
